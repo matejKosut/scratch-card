26b7110bb8ba3c5ef1552eaace2815e6fce50f02
Matej Košút <matej.kosut21@gmail.com>
2024-08-02 11:24:29 +0200

    Cleanup

--- app/src/main/java/sk/matejkosut/scratchcard/util/CoroutinesUtils.kt
+++
@@ -1,6 +0,0 @@
-package sk.matejkosut.scratchcard.util
-
-import kotlinx.coroutines.flow.SharingStarted
-
-private const val StopTimeoutMillis: Long = 5000
-val WhileUiSubscribed: SharingStarted = SharingStarted.WhileSubscribed(StopTimeoutMillis)
